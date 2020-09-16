fb0a81a255e2c81829b3d64c180472448dc2b9f1
Valery <lekalera777@gmail.com>
2020-09-16 17:16:21 +0300

    change functional (addind completed tasks to the list end, counting tasks)/change font

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ Minimal TODO list web app on native JS:
 + Easy to use.
 + Allows people to update a list and save it.
 + Mobile friendly.
+
+
 View demo: https://valery-sergeevna.github.io/todo-list/
 
--- jsStudy.js
+++ jsStudy.js
@@ -3,7 +3,10 @@ window.addEventListener('DOMContentLoaded', () => {
 		list = document.querySelector('.tasks'),
 		btn = document.querySelector('.add'),
 		btnClear = document.querySelector('.clear'),
-		btnSave = document.querySelector('.save');
+		btnSave = document.querySelector('.save'),
+		countAll = document.querySelector('#all'),
+		countDone = document.querySelector('#done'),
+		countNotDone = document.querySelector('#not-done');
 
 	//create to-do task
 	const createTasks = () => {
@@ -27,24 +30,55 @@ window.addEventListener('DOMContentLoaded', () => {
 		li.append(deleteBtn);
 		input.value = "";
 		deleteTask(deleteBtn);
-		makeTaskDone();
+		countTasks();
+		countTasksDone();
+		countUncompletedTasks();
+		addToTheEnd();
 	};
 
+	const addToTheEnd = () => {
+		document.querySelectorAll('li.checked').forEach(li => {
+			list.insertAdjacentElement('beforeend', li);
+		});
+	};
+	//count all tasks
+	const countTasks = () => {
+		countAll.textContent = document.querySelectorAll('.todo-item').length;
+	};
+	//count tasks done
+	const countTasksDone = () => {
+		countDone.textContent = document.querySelectorAll('li.checked').length;
+	};
+	//count uncompleted tasks
+	const countUncompletedTasks = () => {
+		countNotDone.textContent = parseInt(countAll.textContent - countDone.textContent);
+	};
 	//delete task
-	const deleteTask = element => {
-		element.addEventListener('click', (event) => {
-			element.parentElement.remove();
+	const deleteTask = elem => {
+		elem.addEventListener('click', (event) => {
+			elem.parentElement.remove();
 			event.stopPropagation();
+			countTasks();
+			countTasksDone();
+			countUncompletedTasks();
 		});
 	};
 
 	//make task done
 	const makeTaskDone = (e) => {
-		if (e.target.tagName === 'LI') {
+		if (e.target.classList.contains('todo-item')) {
 			e.target.classList.toggle('checked');
 			e.target.classList.toggle('done');
 		}
+		if (e.target.classList.contains('todo-text')) {
+			e.target.parentElement.classList.toggle('checked');
+			e.target.parentElement.classList.toggle('done');
+		}
+		countTasksDone();
+		countUncompletedTasks();
+		addToTheEnd();
 	};
+
 	//load tasks with using local storage
 	function loadTasks() {
 		if (localStorage.getItem("list")) {
@@ -53,7 +87,10 @@ window.addEventListener('DOMContentLoaded', () => {
 		const deleteButtons = document.querySelectorAll(".todo-trash");
         for (const button of deleteButtons) {
             deleteTask(button);
-        }
+		}
+		countTasks();
+		countTasksDone();
+		countUncompletedTasks();
 	}
 
 	//save tasks in local storage
@@ -66,6 +103,9 @@ window.addEventListener('DOMContentLoaded', () => {
 		document.querySelectorAll('li').forEach(li => {
 			li.remove();
 		});
+		countTasks();
+		countTasksDone();
+		countUncompletedTasks();
 		localStorage.removeItem('list', list.innerHTML);
 	});
 
@@ -77,5 +117,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	btn.addEventListener('click', createTasks);
 	list.addEventListener('click', makeTaskDone);
+	
 	loadTasks();
 });
